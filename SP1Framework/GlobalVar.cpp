26334c3ceaabcf4b3703afca0a7146f8e2a7c487
KneeGah <190874h@mymail.nyp.edu.sg>
2020-08-24 10:52:15 +0800

    Fixed the hydrant to its place

--- SP1Framework/GlobalVar.cpp
+++ SP1Framework/GlobalVar.cpp
@@ -4,6 +4,6 @@
 
 Entity* lvlmanager[20] = {nullptr,};  //creates an array of 20 and fills it up with nullptr
 
-Entity* tut_objects[20] = { new firehydrant(28,12), new firehydrant(52,12), nullptr, };
+Entity* tut_objects[20] = { new firehydrant(27,12), new firehydrant(51,12), nullptr, };
 
 Entity** lvl_array[5] = { tut_objects, nullptr, };
