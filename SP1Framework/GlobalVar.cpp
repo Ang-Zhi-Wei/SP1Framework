b6621d45b49f0fad157ec93e90fb5c7f82450f82
KneeGah <190874h@mymail.nyp.edu.sg>
2020-08-25 15:51:55 +0800

    spawned more enemies

--- SP1Framework/GlobalVar.cpp
+++ SP1Framework/GlobalVar.cpp
@@ -5,6 +5,6 @@
 
 Entity* lvlmanager[20] = {nullptr,};  //creates an array of 20 and fills it up with nullptr
 
-Entity* tut_objects[20] = { new firehydrant(27,12), new firehydrant(51,12), new Enemy(23,15),new Enemy(50,15),new Enemy(57,4),nullptr, };
+Entity* tut_objects[20] = { new firehydrant(27,12), new firehydrant(51,12), new Enemy(23,15),new Enemy(50,15),new Enemy(57,4),new Enemy(20,7),nullptr, };
 
 Entity** lvl_array[5] = { tut_objects, nullptr, };
