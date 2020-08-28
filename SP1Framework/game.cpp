b411085a628baf022f871a857eb92753ce9e69f9
JianYong-Collab <69342160+JianYong-Collab@users.noreply.github.com>
2020-08-28 14:58:04 +0800

    Fixed duplicated portal files

--- SP1Framework/game.cpp
+++ SP1Framework/game.cpp
@@ -1535,135 +1535,113 @@ void potentialmainmenu(void){
 }
 void portalcheck(void) {
     //if number of enemies==0...
-    if (Enemy::GetTotalEnemy()== 0)
-    if (Tutorial == true) {
-        Portal portal;
-        portal.SetPositionX(79);
-        portal.SetPositionY(7);
-        c.X = portal.GetPositionX();
-        c.Y = portal.GetPositionY();
-        //display portal
-        g_Console.writeToBuffer(c, " ", 0xFF);
-        
-        //if player reach portal....
-        if ((g_sChar.m_cLocation.X == portal.GetPositionX()) && (g_sChar.m_cLocation.Y == portal.GetPositionY()))
-        {
-            portal.~Portal();
+    if (Enemy::GetTotalEnemy() == 0)
+        if (Tutorial == true) {
+            Portal portal;
+            portal.SetPositionX(79);
+            portal.SetPositionY(7);
+            c.X = portal.GetPositionX();
+            c.Y = portal.GetPositionY();
+            //display portal
+            g_Console.writeToBuffer(c, " ", 0xFF);
+
+            //if player reach portal....
+            if ((g_sChar.m_cLocation.X == portal.GetPositionX()) && (g_sChar.m_cLocation.Y == portal.GetPositionY()))
+            {
+                portal.~Portal();
+            }
+
         }
-        
-    }
-    else if (level1 == true) {
-        Portal portal;
-        portal.SetPositionX(79);
-        portal.SetPositionY(11);
-        c.X = portal.GetPositionX();
-        c.Y = portal.GetPositionY();
-        //display portal
-        g_Console.writeToBuffer(c, " ", 0xFF);
+        else if (level1 == true) {
+            Portal portal;
+            portal.SetPositionX(79);
+            portal.SetPositionY(11);
+            c.X = portal.GetPositionX();
+            c.Y = portal.GetPositionY();
+            //display portal
+            g_Console.writeToBuffer(c, " ", 0xFF);
 
-        //if player reach portal....
-        if ((g_sChar.m_cLocation.X == portal.GetPositionX()) && (g_sChar.m_cLocation.Y == portal.GetPositionY()))
-        {
-            portal.~Portal();
-        }
-    }
-    else if (level2 == true) {
-        Portal portal;
-        portal.SetPositionX(38);
-        portal.SetPositionY(13);
-        c.X = portal.GetPositionX();
-        c.Y = portal.GetPositionY();
-        //display portal
-        g_Console.writeToBuffer(c, " ", 0xFF);
-
-        //if player reach portal....
-        if ((g_sChar.m_cLocation.X == portal.GetPositionX()) && (g_sChar.m_cLocation.Y == portal.GetPositionY()))
-        {
-            portal.~Portal();
-        }
-    }
-    else if (level3 == true) {
-        Portal portal , portal2;
-        portal.SetPositionX(72);
-        portal.SetPositionY(0);
-        portal2.SetPositionX(73);
-        portal2.SetPositionY(24);
-        c.X = portal.GetPositionX();
-        c.Y = portal.GetPositionY();
-        g_Console.writeToBuffer(c, " ", 0xFF);
-        //display portal
-        c.X = portal2.GetPositionX();
-        c.Y = portal2.GetPositionY();
-        //display portal 2
-        
-        g_Console.writeToBuffer(c, " ", 0xFF);
-
-        //if player reach portal....
-        if ((g_sChar.m_cLocation.X == portal.GetPositionX()) && (g_sChar.m_cLocation.Y == portal.GetPositionY()))
-        {
-            momstatus = true;
-            portal.~Portal();
-            portal2.~Portal();
-        }
-        if ((g_sChar.m_cLocation.X == portal2.GetPositionX()) && (g_sChar.m_cLocation.Y == portal2.GetPositionY()))
-        {
-            dadstatus = true;
-            portal.~Portal();
-            portal2.~Portal();
-        }
-    }
-    else if (level4 == true && momstatus == true) {
-        Portal portal;
-        portal.SetPositionX(79);
-        portal.SetPositionY(7);
-        c.X = portal.GetPositionX();
-        c.Y = portal.GetPositionY();
-        //display portal
-        g_Console.writeToBuffer(c, " ", 0xFF);
-
-        //if player reach portal....
-        if ((g_sChar.m_cLocation.X == portal.GetPositionX()) && (g_sChar.m_cLocation.Y == portal.GetPositionY()))
-        {
-            portal.~Portal();
-        }
-    }
-    else if (level4 == true && dadstatus == true) {
-        Portal portal;
-        portal.SetPositionX(79);
-        portal.SetPositionY(7);
-        c.X = portal.GetPositionX();
-        c.Y = portal.GetPositionY();
-        //display portal
-        g_Console.writeToBuffer(c, " ", 0xFF);
-
-        //if player reach portal....
-        if ((g_sChar.m_cLocation.X == portal.GetPositionX()) && (g_sChar.m_cLocation.Y == portal.GetPositionY()))
-        {
-            portal.~Portal();
-            if (Enemy::GetTotalEnemy == 0) {
-                if (Tutorial == true) {
-                    //display portal
-                    //if player reach portal....
-                }
-                else if (level1 == true) {
-
-                }
-                else if (level2 == true) {
-
-                }
-                else if (level3 == true) {
-
-                }
-                else if (level4 == true && momstatus == true) {
-
-                }
-                else if (level4 == true && dadstatus == true) {
-
-                }
+            //if player reach portal....
+            if ((g_sChar.m_cLocation.X == portal.GetPositionX()) && (g_sChar.m_cLocation.Y == portal.GetPositionY()))
+            {
+                portal.~Portal();
             }
         }
-    }
-   
+        else if (level2 == true) {
+            Portal portal;
+            portal.SetPositionX(38);
+            portal.SetPositionY(13);
+            c.X = portal.GetPositionX();
+            c.Y = portal.GetPositionY();
+            //display portal
+            g_Console.writeToBuffer(c, " ", 0xFF);
+
+            //if player reach portal....
+            if ((g_sChar.m_cLocation.X == portal.GetPositionX()) && (g_sChar.m_cLocation.Y == portal.GetPositionY()))
+            {
+                portal.~Portal();
+            }
+        }
+        else if (level3 == true) {
+            Portal portal, portal2;
+            portal.SetPositionX(72);
+            portal.SetPositionY(0);
+            portal2.SetPositionX(73);
+            portal2.SetPositionY(24);
+            c.X = portal.GetPositionX();
+            c.Y = portal.GetPositionY();
+            g_Console.writeToBuffer(c, " ", 0xFF);
+            //display portal
+            c.X = portal2.GetPositionX();
+            c.Y = portal2.GetPositionY();
+            //display portal 2
+
+            g_Console.writeToBuffer(c, " ", 0xFF);
+
+            //if player reach portal....
+            if ((g_sChar.m_cLocation.X == portal.GetPositionX()) && (g_sChar.m_cLocation.Y == portal.GetPositionY()))
+            {
+                momstatus = true;
+                portal.~Portal();
+                portal2.~Portal();
+            }
+            if ((g_sChar.m_cLocation.X == portal2.GetPositionX()) && (g_sChar.m_cLocation.Y == portal2.GetPositionY()))
+            {
+                dadstatus = true;
+                portal.~Portal();
+                portal2.~Portal();
+            }
+        }
+        else if (level4 == true && momstatus == true) {
+            Portal portal;
+            portal.SetPositionX(79);
+            portal.SetPositionY(7);
+            c.X = portal.GetPositionX();
+            c.Y = portal.GetPositionY();
+            //display portal
+            g_Console.writeToBuffer(c, " ", 0xFF);
+
+            //if player reach portal....
+            if ((g_sChar.m_cLocation.X == portal.GetPositionX()) && (g_sChar.m_cLocation.Y == portal.GetPositionY()))
+            {
+                portal.~Portal();
+            }
+        }
+        else if (level4 == true && dadstatus == true) {
+            Portal portal;
+            portal.SetPositionX(79);
+            portal.SetPositionY(7);
+            c.X = portal.GetPositionX();
+            c.Y = portal.GetPositionY();
+            //display portal
+            g_Console.writeToBuffer(c, " ", 0xFF);
+            //if player reach portal....
+            if ((g_sChar.m_cLocation.X == portal.GetPositionX()) && (g_sChar.m_cLocation.Y == portal.GetPositionY()))
+            {
+                portal.~Portal();
+            }
+
+        }
 }
 void liveordeathstatus(void) 
 {
